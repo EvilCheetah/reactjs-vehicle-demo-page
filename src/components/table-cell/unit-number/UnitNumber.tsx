c39f879196aa4517c367b1de7a6a94fd91a2d975
Eugene M <eugene.moschyn@gmail.com>
2022-09-17 20:56:13 -0700

    add(): tooltip explaining

--- src/components/table-cell/unit-number/UnitNumber.tsx
+++ src/components/table-cell/unit-number/UnitNumber.tsx
@@ -1,12 +1,28 @@
 import './UnitNumber.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKey, faLock, faRoadLock } from '@fortawesome/free-solid-svg-icons';
+import PlannedToolTip from '../../tool-tip/PlannedToolTip';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
+import Reserved from './Reserved';
+import { Reserve } from '../../../interface/reserve.interface';
 
 
-function UnitNumber({ unit_number, is_active, is_reserved }: { unit_number: string, is_active: boolean, is_reserved: boolean })
+function UnitNumber({ unit_number, is_active, reserve, pre_booked }: { unit_number: string, is_active: boolean, reserve: Reserve, pre_booked: boolean })
 {
     return (
         <td className='unit-information'>
-            <div className={`unit-number ${is_active ? 'active-unit' : 'not-active-unit'}`}>{unit_number}</div>
-            <div className={`is-reserved ${is_reserved ? 'reserved' : 'not-reserved'}`}>{is_reserved ? '📕' : '📗'}</div>   
+            <div className={`unit-number ${is_active ? 'active-unit' : 'not-active-unit'}`}>{unit_number}</div> 
+            <Reserved reserve={reserve} />           
+            {
+                pre_booked &&
+                <Tooltip 
+                    title={<PlannedToolTip pro_number="AAA" owner="Arman A."/>}
+                    placement="top"
+                    arrow
+                >
+                <div className="pre-booked">Planned</div>
+                </Tooltip>
+}
         </td>
     );
 }
