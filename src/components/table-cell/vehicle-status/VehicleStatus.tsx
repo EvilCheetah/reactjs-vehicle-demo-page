34b86e2dd26a576be238714bd84f634edef33c60
Eugene M <eugene.moschyn@gmail.com>
2022-09-17 20:21:54 -0700

    add(): better classes for styling

--- src/components/table-cell/vehicle-status/VehicleStatus.tsx
+++ src/components/table-cell/vehicle-status/VehicleStatus.tsx
@@ -1,14 +1,14 @@
 import './VehicleStatus.css';
 import { StatusType } from "../../../types/status.type";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRoadLock } from '@fortawesome/free-solid-svg-icons';
 
 
-function VehicleStatus({ status }: { status: StatusType })
+function VehicleStatus({ status, pre_booked }: { status: StatusType, pre_booked: boolean })
 {
     return (
-        <td 
-            className={`status ${status.toLowerCase().replaceAll(' ', '-')}`}
-        >
-            { status }
+        <td>
+            <div className={`status ${status.toLowerCase().replaceAll(' ', '-')}`}>{ status }</div>
         </td>
     );
 }
