b686395e7f6bb29fabd2abb36279376be11382ca
Eugene M <eugene.moschyn@gmail.com>
2022-09-17 20:53:04 -0700

    update(): interval implementation

--- src/components/table-cell/vehicle-location/VehicleLocation.tsx
+++ src/components/table-cell/vehicle-location/VehicleLocation.tsx
@@ -1,14 +1,14 @@
 import './VehicleLocation.css';
-import { DateTime } from "luxon";
+import { DateTime, Interval } from "luxon";
 import { Status } from "../../../interface/status.interface";
 
 
 function VehicleLocation({ location }: { location: Status })
 {
     // const [city, state] = location.location.split(', ');
-    const date = DateTime.fromISO(location.last_updated).setZone("America/Los_Angeles");
+    const date     = DateTime.fromISO(location.last_updated).setZone("America/Los_Angeles");
     const date_now = DateTime.now().setZone("America/Los_Angeles");
-    const {months} = date_now.diff(date, ['months']).toObject();
+    const months   = Interval.fromDateTimes(date, date_now).length('months');
 
     return (
         <td className="location">
