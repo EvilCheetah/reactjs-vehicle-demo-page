7020bcd0882a9a7a2ca3647a3d269e4b8bc346d7
Eugene M <eugene.moschyn@gmail.com>
2023-06-09 02:24:24 -0700

    fix(): base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'ReactJS-Vehicle-Tab',
+  base: 'reactjs-vehicle-demo-page',
   plugins: [react()]
 })
